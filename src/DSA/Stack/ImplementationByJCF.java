f0500fb76ec0558b608e282189b03060c97fca8a
PANKAJ BHATT <211pankajbhatt@gmail.com>
2025-10-03 11:54:47 +0530

    implementation of Stack by java collection framework

---
+++ src/DSA/Stack/ImplementationByJCF.java
@@ -0,0 +1,28 @@
+package DSA.Stack;
+
+import java.util.*;
+
+public class ImplementationByJCF {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        s.push(7);
+        s.push(8);
+        s.push(9);
+        s.push(10);
+        s.push(11);
+        s.push(12);
+        s.push(13);
+        s.push(14);
+        s.push(15);
+        
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+}
