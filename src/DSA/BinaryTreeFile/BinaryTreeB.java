50e10524989bf412ecb9af972f562c9928e74645
PANKAJ BHATT <211pankajbhatt@gmail.com>
2025-10-04 09:16:33 +0530

    added method on binary tree height of a tree

--- src/DSA/BinaryTreeFile/BinaryTreeB.java
+++ src/DSA/BinaryTreeFile/BinaryTreeB.java
@@ -99,6 +99,13 @@ public class BinaryTreeB {
             
             
         }
+        
+        public int heightOfTree(Node root){
+            if(root == null){
+                return 0;
+            }
+           return 1+Math.max(heightOfTree(root.left) , heightOfTree(root.right));
+        }
     }
     
     
@@ -107,5 +114,7 @@ public class BinaryTreeB {
         BinaryTree tree = new BinaryTree();
         Node root = tree.TreeBuilder(nodes);
         tree.levelOrder(root);
+        System.out.println(tree.heightOfTree(root));
+
     }
 }
